a2c90032fce4aeeef8fbcf6e3fae6300f5ac3ab5
Tyler Lum <tylergwlum@gmail.com>
2021-06-06 15:47:19 -0700

    Almost done refactor

--- MyFunctions.py
+++ MyFunctions.py
@@ -12,17 +12,7 @@
 #     name: python3
 # ---
 
-# +
-from copy import deepcopy
-
-from openpyxl.styles import PatternFill
-
-redFill = PatternFill(start_color='FFFF0000',
-                   end_color='FFFF0000',
-                   fill_type='solid')
-blueFill = PatternFill(start_color='00FFFF',
-                   end_color='00FFFF',
-                   fill_type='solid')
+from copy import copy
 
 # +
 ## Low-level functions ##
@@ -40,14 +30,6 @@ Returns list of these strings
 import re
 def split(string):
     return re.split(', |; |\. |  ', string)
-
-"""
-Add column with to sheet to max column of sheet
-Labels first row with column_header
-"""
-def add_column(sheet, column_header):
-    max_col = sheet.max_column
-    sheet.cell(row=1, column=max_col+1).value = column_header
 
 """
 Returns True if statement both
@@ -87,79 +69,14 @@ def contains(statement, keywords):
     return False
 
 """
-Shortens diagnostic_type
-admission => addx
-discharge => dcdx
-"""
-def get_shortened_diagnostic_type(diagnostic_type):
-    mapping = {"admission": "addx", "discharge": "dcdx"}
-    return mapping[diagnostic_type]
-
-"""
-Create a dictionary of column names
-"""   
-def get_column_names(sheet):
-    column_names = {}
-    i = 0
-    for i, col in enumerate(sheet.iter_cols(1, sheet.max_column)):
-        column_names[col[0].value] = i
-    return column_names
-
-"""
 Handle edge cases of illnesses
 Return fixed illnesses list
 """
 def manipulate_illness_list(illnesses):
-    illnesses = deepcopy(illnesses)
+    illnesses = copy(illnesses)
 
     ## EOS/Personality
     if "Schizophrenia Spectrum and Other Psychotic Disorders" in illnesses and "Personality" in illnesses:
         illnesses.remove("Schizophrenia Spectrum and Other Psychotic Disorders")      
     
     return illnesses
-
-"""
-Input list of strings
-Split each string and add to list
-Return list of all splits
-"""
-def combine_split_strings(strings):
-    actual_strings = [s for s in strings if s is str]
-
-    split_up_strings = []
-    for s in actual_strings:
-        split_up_strings += split(s.lower())
-    return split_up_strings
-
-
-# -
-
-## Module 5: Read all four info columns, One hot encode symptoms
-"""
-For each symptom in diagnosis, label the corresponding column with a 1
-"""
-def ohe_symptoms(sheet, ignore_keywords, symptom_keywords):
-    column_names = get_column_names(sheet)
-    
-    for row_cells in sheet.iter_rows(min_row=2, max_row=sheet.max_row):
-        
-        ## Get all diagnosis info
-        ref_reason = row_cells[column_names['ref_reason']].value
-        chief_complaint = row_cells[column_names['chief_complaint']].value
-        admission_diagnosis = row_cells[column_names['admission_diagnosis']].value
-        discharge_diagnosis = row_cells[column_names['discharge_diagnosis']].value
-        
-        all_info = []
-        all_info.extend((ref_reason, chief_complaint, admission_diagnosis, discharge_diagnosis))
-        
-        split_all_info = combine_split_strings(all_info)
-        
-        all_reasons = []
-        for info in split_all_info:
-            info_reasons = []
-            ## add all symptoms associated with each statement
-            for symptom in symptom_keywords:
-                if not contains(info, ignore_keywords) and contains(info, symptom_keywords[symptom]):
-                    info_reasons.append(symptom)
-                    row_cells[column_names['{}'.format(symptom)]].value = 1
-            all_reasons = all_reasons + info_reasons
--- RICAP_Diagnoses.py
+++ RICAP_Diagnoses.py
@@ -30,7 +30,7 @@ def is_nan(val):
 
 def convert_to_dict_of_lists_no_nans(df):
     dict_of_lists = df.to_dict(orient='list')
-    
+
     # Remove nans
     for key in dict_of_lists.keys():
         list_ = dict_of_lists[key]
@@ -53,83 +53,86 @@ REASONS_FOR_REFERRAL = list(REASON_FOR_REFERRAL_TO_KEYWORDS.keys())
 # -
 
 ## Setup important words
-DIAGNOSTIC_TYPES = ['admission', 'discharge']
-SHORT_DIAGNOSTIC_TYPES = ['addx', 'dcdx']
+FULL_DIAGNOSTIC_TYPES = ['admission', 'discharge']
+DIAGNOSTIC_TYPES = ['addx', 'dcdx']
 
 # +
 ## Module 1: Add columns headers and set binary values to 0
 
 # Non-binary column headers
-for dt in SHORT_DIAGNOSTIC_TYPES:
+for dt in DIAGNOSTIC_TYPES:
     data_df[f"main_{dt}"] = ""
 
 # Binary column headers
-illness_headers = [f"{dt}_{illness}" for dt in SHORT_DIAGNOSTIC_TYPES for illness in ILLNESSES]
+illness_headers = [f"{dt}_{illness}" for dt in DIAGNOSTIC_TYPES for illness in ILLNESSES]
 mrr_headers = [f"mrr_{reason}" for reason in REASONS_FOR_REFERRAL]
 # symptom_headers = [f"{symptom}" for symptom in SYMPTOMS]
 binary_column_headers = illness_headers + mrr_headers  # + symptom_headers
-    
+
 for column_header in binary_column_headers:
     data_df[column_header] = 0
 # -
 
 ## Module 2: Fill in main diagnosis
-for full_dt, dt in zip(DIAGNOSTIC_TYPES, SHORT_DIAGNOSTIC_TYPES):
+for full_dt, dt in zip(FULL_DIAGNOSTIC_TYPES, DIAGNOSTIC_TYPES):
     for i, row in data_df.iterrows():
-        diagnosis_str = row[f'{full_dt}_diagnosis']
+        diagnosis_statement_str = row[f'{full_dt}_diagnosis']
 
         # Skip nans
-        if is_nan(diagnosis_str):
+        if is_nan(diagnosis_statement_str):
             continue
 
-        # Get main diagnosis by iterating through diagnosis_list until we get a valid diagnosis
-        diagnosis_list = split(diagnosis_str.lower())
+        # Get main diagnosis by iterating through diagnosis_statements until we get a valid statement
+        diagnosis_statements = split(diagnosis_statement_str.lower())
         all_illnesses = []
-        for diagnosis in diagnosis_list:
+        for diagnosis_statement in diagnosis_statements:
+            # Skip statements with IGNORE_KEYWORDS
+            if contains(diagnosis_statement, IGNORE_KEYWORDS):
+                continue
+
             # Get illnesses
-            illnesses = [illness for illness in ILLNESSES
-                         if not contains(diagnosis, IGNORE_KEYWORDS) and
-                         contains(diagnosis, ILLNESS_TO_KEYWORDS[illness])]
-            
+            illnesses = [illness for illness, keywords in ILLNESS_TO_KEYWORDS.items()
+                         if contains(diagnosis_statement, keywords)]
             illnesses = manipulate_illness_list(illnesses)  # Handle edge cases
-            
+
             # Should only be 1 illness, but put in list otherwise
-            # Break after first one is found, as this is only looking for the main diagnosis
+            # Break after first one is found, as we are only looking for the main diagnosis
             if len(illnesses) == 1:
                 data_df.at[i, f'main_{dt}'] = illnesses[0]
                 break
             elif len(illnesses) >= 1:
                 data_df.at[i, f'main_{dt}'] = str(illnesses)
                 break
-            
             # If none found, look at the next diagnosis
             else:  # len(illnesses) == 0
                 continue
 
-## Module 3: Read diagnoses, one hot encode symptoms
-for full_dt, dt in zip(DIAGNOSTIC_TYPES, SHORT_DIAGNOSTIC_TYPES):
+## Module 3: Read diagnoses, one hot encode illnesses
+for full_dt, dt in zip(FULL_DIAGNOSTIC_TYPES, DIAGNOSTIC_TYPES):
     for i, row in data_df.iterrows():
-        diagnosis_str = row[f'{full_dt}_diagnosis']
+        diagnosis_statement_str = row[f'{full_dt}_diagnosis']
 
         # Skip nans
-        if is_nan(diagnosis_str):
+        if is_nan(diagnosis_statement_str):
             continue
 
-        # Get main diagnosis by iterating through diagnosis_list until we get a valid diagnosis
-        diagnosis_list = split(diagnosis_str.lower())
-        for diagnosis in diagnosis_list:
-            # Get illnesses
-            illnesses = [illness for illness in ILLNESSES
-                         if not contains(diagnosis, IGNORE_KEYWORDS) and
-                         contains(diagnosis, ILLNESS_TO_KEYWORDS[illness])]
+        # Get illnesses by iterating through diagnosis_statements
+        diagnosis_statements = split(diagnosis_statement_str.lower())
+        for diagnosis_statement in diagnosis_statements:
+            # Skip statements with IGNORE_KEYWORDS
+            if contains(diagnosis_statement, IGNORE_KEYWORDS):
+                continue
 
+            # Get illnesses
+            illnesses = [illness for illness, keywords in ILLNESS_TO_KEYWORDS.items()
+                         if contains(diagnosis_statement, keywords)]
             illnesses = manipulate_illness_list(illnesses)  # Handle edge cases
 
             # Populate the binary illness values in the df
             for illness in illnesses:
                 data_df.at[i, f'{dt}_{illness}'] = 1
 
-## Module 4: Read ref_reason + chief_complaint, one hot encode symptoms
+## Module 4: Read ref_reason + chief_complaint, one hot encode mrr
 for i, row in data_df.iterrows():
     str1 = row['ref_reason']
     str2 = row['chief_complaint']
@@ -138,25 +141,27 @@ for i, row in data_df.iterrows():
     if is_nan(str1) and is_nan(str2):
         continue
     elif is_nan(str1):
-        ref_reason_str = str2
+        ref_reason_statement_str = str2
     elif is_nan(str2):
-        ref_reason_str = str1
+        ref_reason_statement_str = str1
     else:
-        ref_reason_str = f"{str1}, {str2}"
+        ref_reason_statement_str = f"{str1}, {str2}"
 
-    # Get main diagnosis by iterating through diagnosis_list until we get a valid diagnosis
-    ref_reason_list = split(ref_reason_str.lower())
-    ref_reasons = []
-    for ref_reason in ref_reason_list:
-        # Get illnesses
-        ref_reasons = [r for r in REASONS_FOR_REFERRAL
-                       if not contains(ref_reason, IGNORE_KEYWORDS) and
-                       contains(ref_reason, REASON_FOR_REFERRAL_TO_KEYWORDS[r])]
-        
+    # Get ref_reasons by iterating through ref_reason_statements
+    ref_reason_statements = split(ref_reason_statement_str.lower())
+    for ref_reason_statement in ref_reason_statements:
+        # Skip statements with IGNORE_KEYWORDS
+        if contains(ref_reason_statement, IGNORE_KEYWORDS):
+            continue
+
+        # Get ref_reasons
+        ref_reasons = [rr for rr, keywords in REASON_FOR_REFERRAL_TO_KEYWORDS.items()
+                       if contains(ref_reason_statement, keywords)]
+
         # Populate the binary illness values in the df
         for rr in ref_reasons:
             data_df.at[i, f'mrr_{rr}'] = 1
-            
+
         # Populate Other if no other reason is given
         if len(ref_reasons) == 0:
             data_df.at[i, 'mrr_Other'] = 1
@@ -166,19 +171,21 @@ for i, row in data_df.iterrows():
 # ohe_symptoms(data_sheet, IGNORE_KEYWORDS, SYMPTOM_KEYWORDS)
 
 for i, row in data_df.iterrows():
-    ref_reason_str = row['ref_reason'].lower()
-    chief_complaint_str = row['chief_complaint'].lower()
-    admission_diagnosis_str = row['admission_diagnosis'].lower()
-    discharge_diagnosis_str = row['discharge_diagnosis'].lower()
-    
-    all_statements = (split(ref_reason_str) + split(chief_complaint_str) +
-                      split(admission_diagnosis_str) + split(discharge_diagnosis_str))
-    
+    ref_reason_statement_str = row['ref_reason'].lower()
+    chief_complaint_statement_str = row['chief_complaint'].lower()
+    admission_diagnosis_statement_str = row['admission_diagnosis'].lower()
+    discharge_diagnosis_statement_str = row['discharge_diagnosis'].lower()
+
+    all_statements = (split(ref_reason_statement_str) +
+                      split(chief_complaint_statement_str) +
+                      split(admission_diagnosis_statement_str) +
+                      split(discharge_diagnosis_statement_str))
+
     for statement is all_statements:
         symptoms = [s for s in SYMPTOMS
                     if not contains(statement, IGNORE_KEYWORDS) and
                     contains(statement, SYMPTOM_TO_KEYWORDS[s])]
-        
+
         for s in symptoms:
             data_df.at[i, str(s)] = 1
 # -
